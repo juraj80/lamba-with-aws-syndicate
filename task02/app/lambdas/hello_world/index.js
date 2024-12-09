f7ad0287ed02f17bb3c788e1d73f4458d0aac6dd
Juraj Klucka <juraj.klucka@alcon.com>
2024-12-09 10:56:20 +0100

    updated bad request string

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -11,7 +11,7 @@ exports.handler = async (event) => {
     statusCode: 400,
     body: JSON.stringify({
       statusCode: 400,
-      message: `Resource not found. Request path: ${event.rawPath}. HTTP method: ${event.requestContext.http.method}`,
+      message: `Bad request syntax or unsupported method. Request path: ${event.rawPath}. HTTP method: ${event.requestContext.http.method}`,
     }),
   };
 
